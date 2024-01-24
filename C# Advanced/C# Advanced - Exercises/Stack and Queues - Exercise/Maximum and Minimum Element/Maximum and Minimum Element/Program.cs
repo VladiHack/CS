0838ff5e7a5a24fc3fc608f93bd8859eb068b4cd
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-01-24 17:16:00 +0200

    C# solutions to problems and competition exercises

---
+++ C# Advanced/C# Advanced - Exercises/Stack and Queues - Exercise/Maximum and Minimum Element/Maximum and Minimum Element/Program.cs
@@ -0,0 +1,30 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int commands = int.Parse(Console.ReadLine());
+        Stack<int> stack = new Stack<int>();
+        for(int i=0;i<commands; i++)
+        {
+            int[] line=Console.ReadLine().Split().Select(int.Parse).ToArray();
+            if (line[0] == 1)
+            {
+                stack.Push(line[1]);
+            }
+            else if (line[0] == 2)
+            {
+                stack.Pop();
+            }
+            else if (line[0] == 3&&stack.Count()!=0)
+            {
+                Console.WriteLine(stack.Max());
+            }
+            else if (line[0]==4&&stack.Count()!=0) 
+            {
+                Console.WriteLine(stack.Min());
+            }
+        }
+        Console.WriteLine(String.Join(", ",stack));
+
+    }
+}
