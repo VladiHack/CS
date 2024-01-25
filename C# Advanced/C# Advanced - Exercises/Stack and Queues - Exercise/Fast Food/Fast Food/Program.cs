19fa44f283666ab99db9d99ba4c42adf1f663a59
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-01-25 16:52:59 +0200

    C# problems from competitions and exercises for practise

---
+++ C# Advanced/C# Advanced - Exercises/Stack and Queues - Exercise/Fast Food/Fast Food/Program.cs
@@ -0,0 +1,31 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int food = int.Parse(Console.ReadLine());
+        int[] orders = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        Queue<int> queue = new Queue<int>();
+        int biggest = -1;
+        for(int i=0; i<orders.Length; i++)
+        {
+            if (food>=orders[i])
+            {
+                biggest = Math.Max(orders[i], biggest);
+                food -= orders[i];
+            }
+            else
+            {
+                queue.Enqueue(orders[i]);
+            }
+        }
+        Console.WriteLine(biggest);
+        if (queue.Count ==0)
+        {
+            Console.WriteLine("Orders complete");
+        }
+        else
+        {
+            Console.WriteLine($"Orders left: {String.Join(" ",queue)}");
+        }
+    }
+}
