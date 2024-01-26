1828e0a4821f049b22371878aa37680c07b7537d
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-01-26 16:27:18 +0200

    C# problems

---
+++ C# Advanced/C# Advanced - Exercises/Stack and Queues - Exercise/5. Fashion Boutique/5. Fashion Boutique/Program.cs
@@ -0,0 +1,29 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        Stack<int> stack= new Stack<int>();
+        int capacity = int.Parse(Console.ReadLine());
+        for(int i=0;i<arr.Length; i++)
+        {
+            stack.Push(arr[i]);
+        }
+        int currentCapacity = capacity;
+        int used = 1;
+        while (stack.Count() != 0)
+        {
+            if(stack.Peek()<=currentCapacity)
+            {
+                currentCapacity -= stack.Pop();
+            }
+            else
+            {
+                used++;
+                currentCapacity = capacity;
+                currentCapacity-=stack.Pop();
+            }
+        }
+        Console.WriteLine(used);
+    }
+}
---
+++ C# Advanced/C# Advanced - Exercises/Stack and Queues - Exercise/Songs Queue/Songs Queue/Program.cs
@@ -0,0 +1,52 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        string[] arr = Console.ReadLine().Split(", ").ToArray();
+        Queue<string> queue = new Queue<string>();  
+        for(int i=0;i<arr.Length; i++)
+        {
+            queue.Enqueue(arr[i]);
+        }
+        while (true)
+        {
+            string cmd = Console.ReadLine();
+            if(String.IsNullOrWhiteSpace(cmd))
+            {
+                break;
+            }
+            if (queue.Count() > 0)
+            {
+                if (cmd.Substring(0, 4) == "Play")
+                {
+                    queue.Dequeue();
+                    if(queue.Count()==0)
+                    {
+                        Console.WriteLine("No more songs!");
+                    }
+                }
+                else if (cmd.Substring(0, 3) == "Add")
+                {
+                    string remaining = "";
+                    for (int i = 4; i < cmd.Length; i++)
+                    {
+                        remaining += cmd[i];
+                    }
+                    if (queue.Contains(remaining) == false)
+                    {
+                        queue.Enqueue(remaining);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{remaining} is already contained!");
+                    }
+                }
+                else if (cmd.Substring(0, 4) == "Show")
+                {
+                    Console.WriteLine(String.Join(", ", queue));
+                }
+            }
+        }
+            
+    }
+}
