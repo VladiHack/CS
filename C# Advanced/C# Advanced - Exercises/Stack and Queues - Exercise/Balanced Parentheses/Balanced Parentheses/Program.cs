adec0115bbd9a34a70def318a11f7bf31b88863c
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-01-27 15:25:46 +0200

    C# problems

--- C# Advanced/C# Advanced - Exercises/Stack and Queues - Exercise/Balanced Parentheses/Balanced Parentheses/Program.cs
+++ C# Advanced/C# Advanced - Exercises/Stack and Queues - Exercise/Balanced Parentheses/Balanced Parentheses/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        string expression = Console.ReadLine();
+        Stack<char> open=new Stack<char>();
+        string ans = "YES";
+        for (int i = 0; i < expression.Length; i++)
+        {
+            if (expression[i] == '(' || expression[i] == '[' || expression[i]=='{')
+            {
+                open.Push(expression[i]);
+            }
+            else if (expression[i]==')')
+            {
+                if(open.Count > 0 && open.Peek()=='(')
+                {
+                    open.Pop();
+                }
+                else
+                {
+                    ans = "NO";
+                    break;
+                }
+            }
+            else if (expression[i]==']')
+            {
+                if(open.Count>0 && open.Peek()=='[')
+                {
+                    open.Pop();
+                }
+                else
+                {
+                    ans = "NO";
+                    break;
+                }
+            }
+            else if (expression[i]=='}')
+            {
+                if(open.Count>0 && open.Peek() =='{')
+                {
+                    open.Pop();
+                }
+                else
+                {
+                    ans = "NO";
+                    break;
+                }
+            }
+        }
+        if(open.Count==0&&ans=="YES")
+        {
+            Console.WriteLine(ans);
+        }
+        else
+        {
+            Console.WriteLine("NO");
+        }
+    }
+}
