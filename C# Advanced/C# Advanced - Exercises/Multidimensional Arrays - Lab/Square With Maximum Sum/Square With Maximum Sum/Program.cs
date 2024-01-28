a23dce325a9852f8737590a754382ccc3cbedd84
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-01-28 11:05:19 +0200

    C# problems
    Square With Maximum Sum

---
+++ C# Advanced/C# Advanced - Exercises/Multidimensional Arrays - Lab/Square With Maximum Sum/Square With Maximum Sum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Numerics;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] par = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+        int rows = par[0]; int cols = par[1];
+        int[,] matrix= new int[rows, cols];
+        for(int i=0; i < rows; i++)
+        {
+            int[] row= Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            for (int s = 0;s < cols; s++)
+            {
+                matrix[i,s]= row[s];
+            }
+        }
+        int maxSum = 0;
+        string res = "";
+        for(int i=0;i<rows-1; i++)
+        {
+            for(int k=0;k<cols-1; k++)
+            {
+                if (matrix[i, k] + matrix[i + 1, k] + matrix[i, k + 1] + matrix[i + 1, k + 1] > maxSum)
+                {
+                    maxSum = matrix[i, k] + matrix[i + 1, k] + matrix[i, k + 1] + matrix[i + 1, k + 1];
+                    res = matrix[i,k]+" " + matrix[i, k+1] + "\n" + matrix[i+1,k]+ " " + matrix[i+1, k+1];
+                }
+            }
+        }
+        Console.WriteLine(res+"\n"+maxSum);
+    }
+   
+}
