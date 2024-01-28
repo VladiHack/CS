276e6204d00ace8cf009360bb554cf8508f4ae65
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-01-28 19:25:41 +0200

    C# problems
    1. Diagonal Difference

--- C# Advanced/C# Advanced - Exercises/Multidimensional Arrays - Exercise/Diagonal Difference/Diagonal Difference/Program.cs
+++ C# Advanced/C# Advanced - Exercises/Multidimensional Arrays - Exercise/Diagonal Difference/Diagonal Difference/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[,] matrix=new int[n,n];
+        int primary = 0;int secondary = 0;
+        for(int i=0;i<n;i++)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            primary += arr[i];
+            secondary += arr[n-i-1];
+        }
+        Console.WriteLine(Math.Abs(primary - secondary));
+    }
+}
