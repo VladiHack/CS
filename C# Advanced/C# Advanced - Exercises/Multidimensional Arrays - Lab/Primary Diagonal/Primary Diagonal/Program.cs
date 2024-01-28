cd3be85b7fe74a06fa495e4279d94a63659e9fd7
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-01-28 10:11:13 +0200

    C# problems
    Primary DIagonal

---
+++ C# Advanced/C# Advanced - Exercises/Multidimensional Arrays - Lab/Primary Diagonal/Primary Diagonal/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int rows = int.Parse(Console.ReadLine());
+        int sum = 0;
+        for(int i=0; i < rows; i++)
+        {
+            int[] row = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            sum += row[i];
+        }
+        Console.WriteLine(sum);
+    }
+}
