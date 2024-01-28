fa8c1ff29cdd796deb639cbde44d67a211c0d87b
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-01-28 09:55:12 +0200

    C# problems
    2. Sum Matrix Columns

---
+++ C# Advanced/C# Advanced - Exercises/Multidimensional Arrays - Lab/Sum Matrix Columns/Sum Matrix Columns/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] parameters = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+        int rows = parameters[0];int cols = parameters[1];
+        int[,] matrix= new int[rows, cols];
+        for(int i=0;i < rows;i++)
+        {
+            int[] row=Console.ReadLine().Split().Select(int.Parse).ToArray();
+            for(int s=0;s<cols;s++)
+            {
+                matrix[i,s] = row[s];
+            }
+        }
+        for(int i=0;i<cols;i++)
+        {
+            int sum = 0;
+            for(int k=0;k<rows;k++)
+            {
+                sum += matrix[k,i];
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
