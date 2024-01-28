00db3b01285b64e69b1910dc172f271fab0dc3cf
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-01-28 19:22:14 +0200

    C# problems

---
+++ C# Advanced/C# Advanced - Exercises/Multidimensional Arrays - Lab/Jagged-Array Modification/Jagged-Array Modification/Program.cs
@@ -0,0 +1,45 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int rows = int.Parse(Console.ReadLine());
+        List<List<int>> list= new List<List<int>>();
+        for(int i=0; i<rows; i++)
+        {
+            List<int> ints=Console.ReadLine().Split().Select(int.Parse).ToList();
+            list.Add(ints);
+        }
+        while (true)
+        {
+            string line=Console.ReadLine();
+            if (line == "END")
+            {
+                foreach (var item in list)
+                {
+                    Console.WriteLine(String.Join(" ", item));
+                }
+                break;
+            }
+            string[] split = line.Split().ToArray();
+            int x = int.Parse(split[1]);int y = int.Parse(split[2]); int operat = int.Parse(split[3]);
+            if(x<0||x>=list.Count)
+            {
+                Console.WriteLine("Invalid coordinates");
+                continue;
+            }
+            else if (list[x].Count<=y||y<0)
+            {
+                Console.WriteLine("Invalid coordinates");
+                continue;
+            }
+            if (split[0]=="Add")
+            {
+                list[x][y] += operat;
+            }
+            else if (split[0]=="Subtract")
+            {
+                list[x][y] -= operat;
+            }
+        }
+    }
+}
---
+++ C# Advanced/C# Advanced - Exercises/Multidimensional Arrays - Lab/Pascal Triangle/Pascal Triangle/Program.cs
@@ -0,0 +1,27 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        long[,] matrix=new long[n,n];
+        for(int i=0;i<n;i++)
+        {
+            matrix[i, 0] = 1;
+        }
+        for (int i = 1; i < n; i++)
+        {
+            for(int s=1;s<=i;s++)
+            {
+                matrix[i, s] = matrix[i-1, s ] + matrix[i - 1, s - 1];
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int k=0;k<=i;k++)
+            {
+                Console.Write(matrix[i,k]+" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
