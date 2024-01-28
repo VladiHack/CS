320a47262b29bdc1363b69d30f2fc382575ea377
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-01-28 09:47:33 +0200

    C#
    1. Sum Matrix Elements

---
+++ C# Advanced/C# Advanced - Exercises/Multidimensional Arrays - Lab/1. Sum Matrix Elements/1. Sum Matrix Elements/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] parameters = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+        int rows = parameters[0];int cols= parameters[1];
+        int sum = 0;
+        for(int i=0;i<rows; i++)
+        {
+            int[] rowElements = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            sum += rowElements.Sum();
+        }
+        Console.WriteLine(rows);
+        Console.WriteLine(cols);
+        Console.WriteLine(sum);
+    }
+}
