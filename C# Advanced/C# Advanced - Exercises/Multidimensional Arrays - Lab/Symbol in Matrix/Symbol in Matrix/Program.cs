a87d627fe23f7ccaf7afac3537b02354971c53f2
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-01-28 10:50:19 +0200

    C#
    Symbol in Matrix

---
+++ C# Advanced/C# Advanced - Exercises/Multidimensional Arrays - Lab/Symbol in Matrix/Symbol in Matrix/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Numerics;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int row = int.Parse(Console.ReadLine());
+        char[,] matrix=new char[row,row];
+        for(int i=0; i < row; i++)
+        {
+            string symbol = Console.ReadLine();
+            for(int s=0;s<row; s++)
+            {
+                matrix[i, s] = symbol[s];
+            }
+        }
+        string read = Console.ReadLine();
+        char searchedSymbol = read[0];
+        for (int i = 0; i < row; i++)
+        {
+            for (int k = 0; k < row; k++)
+            { 
+               
+                if (searchedSymbol == matrix[i,k])
+                {
+                    Console.WriteLine($"({i}, {k})");
+                    return;
+                }
+               
+            }
+        }
+        Console.WriteLine($"{searchedSymbol} does not occur in the matrix");
+    }
+}
