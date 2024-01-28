df182ab83eba0e6aae0d14038136c81842cb697b
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-01-28 09:04:22 +0200

    C# problems

---
+++ C# Advanced/C# Advanced - Exercises/Stack and Queues - Exercise/Key Revolver/Key Revolver/Program.cs
@@ -0,0 +1,49 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int bulletPrice = int.Parse(Console.ReadLine());
+        int bulletCapacity=int.Parse(Console.ReadLine());
+        int[] bulletsArr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int[] locksArr= Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int intelligence=int.Parse(Console.ReadLine());
+
+        Stack<int> bullets=new Stack<int>();
+        for(int i = 0; i < bulletsArr.Length; i++)
+        {
+            bullets.Push(bulletsArr[i]);
+        }
+
+        Queue<int> locks=new Queue<int>();
+        for (int i = 0; i < locksArr.Length; i++)
+        {
+            locks.Enqueue(locksArr[i]);
+        }
+
+        while (locks.Count > 0 && bullets.Count > 0)
+        {   
+            if(bullets.Peek()<=locks.Peek())
+            {
+                Console.WriteLine("Bang!");
+                locks.Dequeue();
+            }
+            else
+            {
+                Console.WriteLine("Ping!");
+            }
+            bullets.Pop();
+            if((bulletsArr.Length-bullets.Count)%bulletCapacity==0&&bullets.Count!=0)
+            {
+                Console.WriteLine("Reloading!");
+            }
+        }
+        if(locks.Count==0)
+        {
+            Console.WriteLine($"{bullets.Count} bullets left. Earned "+"$"+(intelligence-(bulletsArr.Length-bullets.Count)*bulletPrice));
+        }
+        else
+        {
+            Console.WriteLine($"Couldn't get through. Locks left: {locks.Count}");
+        }
+    }
+}
