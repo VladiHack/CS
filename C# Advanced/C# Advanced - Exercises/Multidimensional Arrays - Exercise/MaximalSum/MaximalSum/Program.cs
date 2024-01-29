8062a63bf7f500740105580613b5e5b9aefcf807
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-01-29 16:35:49 +0200

    C# problems
    Maximal Sum and Squares in Matrix

---
+++ C# Advanced/C# Advanced - Exercises/Multidimensional Arrays - Exercise/MaximalSum/MaximalSum/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maximal_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] parameters = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int rows = parameters[0]; int cols = parameters[1];
+            long[,] matrix = new long[rows, cols];
+            for (int i = 0; i < rows; i++)
+            {
+                long[] row = Console.ReadLine().Split().Select(long.Parse).ToArray();
+                for (int s = 0; s < row.Length; s++)
+                {
+                    matrix[i, s] = row[s];
+                }
+            }
+            long maxSum = 0;
+            string res = "";
+            for (int i = 0; i < rows - 2; i++)
+            {
+
+                for (int s = 0; s < cols - 2; s++)
+                {
+                    long sum = matrix[i, s] + matrix[i + 1, s] + matrix[i + 2, s] + matrix[i, s + 1] + matrix[i, s + 2] + matrix[i + 1, s + 1] + matrix[i + 1, s + 2] + matrix[i + 2, s + 1] + matrix[i + 2, s + 2];
+
+                    if (sum > maxSum)
+                    {
+                        maxSum = sum;
+                        res = matrix[i, s] + " " + matrix[i, s + 1] + " " + matrix[i, s + 2] + "\n" + matrix[i + 1, s] + " " + matrix[i + 1, s + 1] + " " + matrix[i + 1, s + 2] + "\n" + matrix[i + 2, s] + " " + matrix[i + 2, s + 1] + " " + matrix[i + 2, s + 2];
+                    }
+                }
+            }
+
+            Console.WriteLine("Sum = " + maxSum);
+            Console.WriteLine(res);
+
+
+        }
+    }
+}
---
+++ C# Advanced/C# Advanced - Exercises/Multidimensional Arrays - Exercise/Squares in Matrix/Squares in Matrix/Program.cs
@@ -0,0 +1,31 @@
+﻿    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] param = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int rows = param[0];int cols = param[1];
+            char[,] matrix=new char[rows,cols];
+            for(int i=0;i<rows; i++)
+            {
+                char[] symbols = Console.ReadLine().Split().Select(char.Parse).ToArray();
+                for(int s=0;s<cols;s++)
+                {
+                    matrix[i,s]= symbols[s];
+                }
+            }
+            int squares = 0;
+            for(int i=0;i<rows-1;i++) 
+            {
+                for(int s=0;s<cols-1;s++)
+                {
+                    if (matrix[i, s] == matrix[i,s+1] && matrix[i, s] == matrix[i+1,s] && matrix[i, s] == matrix[i+1,s+1])
+                    {
+                        squares++;
+                    }
+                }
+            }
+            Console.WriteLine(squares);
+        }
+    }
