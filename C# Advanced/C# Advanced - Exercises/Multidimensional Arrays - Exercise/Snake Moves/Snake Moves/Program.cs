d40c6452474de7638695f2b7f5ac89df2658c686
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-01-29 17:07:11 +0200

    C# problems
    Snake moves

---
+++ C# Advanced/C# Advanced - Exercises/Multidimensional Arrays - Exercise/Snake Moves/Snake Moves/Program.cs
@@ -0,0 +1,41 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] parameters = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int rows = parameters[0];int cols= parameters[1];
+        string snakeSize = Console.ReadLine();
+        int counter = -1;
+        for(int i=0;i<rows; i++)
+        {
+            if(i%2==0)
+            {
+                for(int k=0;k<cols;k++)
+                {
+                    counter++;
+                    if(counter==snakeSize.Length)
+                    {
+                        counter = 0;
+                    }
+                    Console.Write(snakeSize[counter]);
+                }
+            }
+            else
+            {
+                string line = "";
+                for (int k=cols-1;k>=0;k--)
+                {
+                    counter++;
+                    if(counter==snakeSize.Length)
+                    {
+                        counter = 0;
+                    }
+                    line += snakeSize[counter];
+                }
+                for(int  j=line.Length-1;j>=0;j--)
+                Console.Write(line[j]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
