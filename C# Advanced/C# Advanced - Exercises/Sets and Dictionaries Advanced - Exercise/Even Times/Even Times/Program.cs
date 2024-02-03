8fb58ffd92e0188243bf170ef7ceb14227da60bf
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-02-03 10:19:59 +0200

    C# problems
    Even times

---
+++ C# Advanced/C# Advanced - Exercises/Sets and Dictionaries Advanced - Exercise/Even Times/Even Times/Program.cs
@@ -0,0 +1,27 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        Dictionary<int,int> timesMet= new Dictionary<int,int>();
+        for(int i=0; i < n; i++)
+        {
+            int num = int.Parse(Console.ReadLine());
+            if(timesMet.ContainsKey(num))
+            {
+                timesMet[num]++;
+            }
+            else
+            {
+                timesMet.Add(num, 1);
+            }
+        }
+        foreach (var item in timesMet)
+        {
+            if(item.Value%2==0)
+            {
+                Console.WriteLine(item.Key);
+            }
+        }
+    }
+}
