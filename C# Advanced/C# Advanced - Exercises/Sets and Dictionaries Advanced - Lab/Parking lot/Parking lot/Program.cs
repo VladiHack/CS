416fa5ea74ded4ff735636eb65cf46a16fc4face
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-02-03 08:56:01 +0200

    C# problems
    Parking Lot

---
+++ C# Advanced/C# Advanced - Exercises/Sets and Dictionaries Advanced - Lab/Parking lot/Parking lot/Program.cs
@@ -0,0 +1,34 @@
+﻿public class Program
+{
+   public static void Main(string[] args)
+    {
+        List<string> list = new List<string>();
+        while(true)
+        {
+            string[] read = Console.ReadLine().Split(", ").ToArray();
+            if (read[0] == "END")
+            {
+                if(list.Count == 0)
+                {
+                    Console.WriteLine("Parking Lot is Empty");
+                }
+                else
+                {
+                    for(int i=0; i<list.Count; i++)
+                    {
+                        Console.WriteLine(list[i]);
+                    }
+                }
+                return;
+            }
+            if (read[0] =="IN" && list.Contains(read[1])==false)
+            {
+                list.Add(read[1]);
+            }
+            else if (list.Contains(read[1]) && read[0]=="OUT")
+            {
+                list.Remove(read[1]);
+            }
+        }
+    }
+}
