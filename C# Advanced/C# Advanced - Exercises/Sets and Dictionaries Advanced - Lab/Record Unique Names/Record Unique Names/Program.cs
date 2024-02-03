2372c220c78adf065704a4a280b4e24ffdf16658
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-02-03 08:48:16 +0200

    C# problems
    Unique names

---
+++ C# Advanced/C# Advanced - Exercises/Sets and Dictionaries Advanced - Lab/Record Unique Names/Record Unique Names/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int names = int.Parse(Console.ReadLine());
+        HashSet<string> uniqueNames=new HashSet<string>();
+        for (int i = 0; i < names; i++)
+        {
+            string readName=Console.ReadLine();
+            uniqueNames.Add(readName);
+        }
+        foreach (var item in uniqueNames)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
