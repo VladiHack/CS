d6a553d546000324c1439c9ef482936f4d0c4483
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-02-03 05:55:47 +0200

    C# problems
    Average Student Grades

---
+++ C# Advanced/C# Advanced - Exercises/Sets and Dictionaries Advanced - Lab/Average Student Grades/Average Student Grades/Program.cs
@@ -0,0 +1,33 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        Dictionary<string, List<double>> dict = new Dictionary<string, List<double>>();
+        for (int i = 0; i < n; i++)
+        {
+            string[] split = Console.ReadLine().Split().ToArray();
+            if (dict.ContainsKey(split[0]))
+            {
+                dict[split[0]].Add(double.Parse(split[1]));
+            }
+            else
+            {
+                List<double> list = new List<double>();
+                list.Add(double.Parse(split[1]));
+                dict.Add(split[0], list);
+            }
+        }
+        foreach (var item in dict)
+        {
+            string name = item.Key;
+            List<double> list = item.Value;
+            Console.Write($"{name} -> ");
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.Write($"{list[i]:F2} ");
+            }
+            Console.WriteLine($"(avg: {list.Average():F2})");
+        }
+    }
+}
---
+++ C# Advanced/C# Advanced - Exercises/Sets and Dictionaries Advanced - Lab/Count Same Values in Array/Count Same Values in Array/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Dictionary<double,int> dict=new Dictionary<double,int>();
+        double[] arr = Console.ReadLine().Split(" ").Select(double.Parse).ToArray();
+        for(int i=0;i<arr.Length; i++)
+        {
+            if (dict.ContainsKey(arr[i]))
+            {
+                dict[arr[i]]++;
+            }
+            else
+            {
+                dict.Add(arr[i], 1);
+            }
+        }
+        foreach(var item in dict)
+        {
+            Console.WriteLine(item.Key + " - " + item.Value + " times");
+        }
+    }
+}
