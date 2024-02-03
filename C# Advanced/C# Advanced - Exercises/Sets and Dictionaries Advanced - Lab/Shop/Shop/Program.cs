2386ad461feaa1af8ddb46e31d4ed76b27f3f635
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-02-03 07:47:52 +0200

    Shop
    C#

---
+++ C# Advanced/C# Advanced - Exercises/Sets and Dictionaries Advanced - Lab/Shop/Shop/Program.cs
@@ -0,0 +1,34 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Dictionary<string,Dictionary<string, double>> shop = new Dictionary<string, Dictionary<string, double>>();
+         while(true)
+        { 
+            string[] split = Console.ReadLine().Split(", ").ToArray();
+            if (split[0] == "Revision")
+            {
+                shop = shop.OrderBy(x => x.Key).ToDictionary(pair=>pair.Key, pair => pair.Value);
+                foreach (var item in shop)
+                {
+                    Console.WriteLine(item.Key + "->");
+                    foreach (var item2 in item.Value)
+                    {
+                        Console.WriteLine($"Product: {item2.Key}, Price: {item2.Value}");
+                    }
+                }
+                return;
+            }
+            if (!shop.ContainsKey(split[0]))
+            {
+                    Dictionary<string, double> products = new Dictionary<string, double>();
+                    shop.Add(split[0], products);
+            }           
+            Dictionary<string, double> prevProducts = shop[split[0]];
+            prevProducts.Add(split[1], double.Parse(split[2]));
+            shop[split[0]] = prevProducts;
+           
+        }
+    }
+ }
+    
