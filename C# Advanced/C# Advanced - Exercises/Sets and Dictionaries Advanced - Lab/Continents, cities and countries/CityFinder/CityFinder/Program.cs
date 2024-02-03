1e817dd28a0162628efe7356836980cf81a812d7
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-02-03 08:40:48 +0200

    C# problems
    Cities and continent by country

---
+++ C# Advanced/C# Advanced - Exercises/Sets and Dictionaries Advanced - Lab/Continents, cities and countries/CityFinder/CityFinder/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int lines = int.Parse(Console.Readline());
+        for(int i = 0; i < lines; i++)
+        {
+            string[] read = Console.Readline().Split().ToArray();
+        }
+    }
+}
---
+++ C# Advanced/C# Advanced - Exercises/Sets and Dictionaries Advanced - Lab/FindCity/FindCity/Program.cs
@@ -0,0 +1,33 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int lines = int.Parse(Console.ReadLine());
+        Dictionary<string, Dictionary<string, List<string>>> map = new Dictionary<string, Dictionary<string, List<string>>>();
+        for(int i=0; i<lines; i++)
+        {
+            string[] read = Console.ReadLine().Split().ToArray();
+            if (!map.ContainsKey(read[0]))
+            {
+                 Dictionary<string,List<string>> keyValueP= new Dictionary<string, List<string>>();
+                 map.Add(read[0], keyValueP);
+            }
+            Dictionary<string, List<string>> current = map[read[0]];
+            if (!current.ContainsKey(read[1]))
+            {
+                List<string> list = new List<string>();
+                current.Add(read[1], list);
+            }
+            current[read[1]].Add(read[2]);
+            map[read[0]] = current;
+        }
+        foreach (var continent in map)
+        {
+            Console.WriteLine(continent.Key+":");
+            foreach (var country in continent.Value)
+            {
+                Console.WriteLine($"{country.Key} -> {string.Join(", ",country.Value)}");
+            }
+        }
+    }
+}
