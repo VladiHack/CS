21839d407fa0f7b9f32bbd3ae045edb368b48d9b
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-02-03 09:22:56 +0200

    C# problems
    party

---
+++ C# Advanced/C# Advanced - Exercises/Sets and Dictionaries Advanced - Lab/Party/Party/Program.cs
@@ -0,0 +1,55 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<string> regular=new List<string>();
+        List<string> vip=new List<string>();
+        bool partyStarted = false;
+        while(true)
+        {
+            string read = Console.ReadLine();
+            if(read=="END")
+            {
+                Console.WriteLine(vip.Count+regular.Count);
+                if (vip.Count>0)
+                {
+                    for(int i=0;i<vip.Count; i++)
+                    {
+                        Console.WriteLine(vip[i]);
+                    }
+                }
+                if(regular.Count>0)
+                {
+                    for(int i=0;i<regular.Count; i++)
+                        Console.WriteLine(regular[i]);
+                }
+                break;
+            }
+            else if(read=="PARTY")
+            {
+                partyStarted = true;
+            }
+            else
+            {
+                if (read[0] >= '0' && read[0]<='9')
+                {
+                    if(vip.Contains(read)==false && partyStarted==false)
+                    {
+                        vip.Add(read);
+                    }
+                    else if(vip.Contains(read)&&partyStarted)
+                    {
+                        vip.Remove(read);
+                    }
+                }
+                else 
+                {
+                    if (regular.Contains(read) == false && partyStarted == false)
+                        regular.Add(read);
+                    else if (regular.Contains(read) && partyStarted)
+                        regular.Remove(read);
+                }
+            }
+        }
+    }
+}
