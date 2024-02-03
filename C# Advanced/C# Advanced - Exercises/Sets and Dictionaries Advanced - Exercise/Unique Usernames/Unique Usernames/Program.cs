040dffbff6275de0a8141b39fadcc82d3f092220
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-02-03 09:33:26 +0200

    C# problems
    Unique Usernames

---
+++ C# Advanced/C# Advanced - Exercises/Sets and Dictionaries Advanced - Exercise/Unique Usernames/Unique Usernames/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int lines = int.Parse(Console.ReadLine());
+        HashSet<string> usernames = new HashSet<string>();
+        for(int i=0; i < lines; i++)
+        {
+            string name= Console.ReadLine();
+            usernames.Add(name);
+        }
+        foreach (var username in usernames)
+        {
+            Console.WriteLine(username);
+        }
+    }
+}
