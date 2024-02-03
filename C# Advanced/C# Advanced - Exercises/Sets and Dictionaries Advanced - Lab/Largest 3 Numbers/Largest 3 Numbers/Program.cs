4c16225a1ad12ce39b1a7c5a2575b310e2eac237
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-02-03 06:11:30 +0200

    C#
    Biggest 3 numbers

---
+++ C# Advanced/C# Advanced - Exercises/Sets and Dictionaries Advanced - Lab/Largest 3 Numbers/Largest 3 Numbers/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<int> list = Console.ReadLine().Split().Select(int.Parse).ToList();
+        list=list.OrderByDescending(x => x).ToList();
+        for(int i=0;i<list.Count; i++)
+        {
+            if (i > 2)
+                break;
+            Console.Write(list[i]+" ");
+        }
+    }
+}
