3cb3a6646c0ef5d4e28dca749a6d96e68691a3ab
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-02-03 10:07:07 +0200

    C#
    Periodic Table

---
+++ C# Advanced/C# Advanced - Exercises/Sets and Dictionaries Advanced - Exercise/Periodic Table/Periodic Table/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        List<string> elements= new List<string>();
+        for(int i=0; i<n; i++)
+        {
+            string[] read = Console.ReadLine().Split().ToArray();
+            for(int s=0;s<read.Length; s++)
+            {
+                if (elements.Contains(read[s])==false)
+                {
+                    elements.Add(read[s]);
+                }
+            }
+        }
+        elements=elements.OrderBy(x => x).ToList();
+        Console.WriteLine(String.Join(" ",elements));
+    }
+}
+          
---
+++ C# Advanced/C# Advanced - Exercises/Sets and Dictionaries Advanced - Exercise/Sets of Elements/Sets of Elements/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] parameters = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = parameters[0];int m = parameters[1];
+        Dictionary<int, int> timesMetFirst=new Dictionary<int, int>();
+        Dictionary<int, int> timesMetSecond= new Dictionary<int, int>();
+
+        for (int i=0;i<n;i++)
+        {
+            int num = int.Parse(Console.ReadLine());
+            if(timesMetFirst.ContainsKey(num))
+            {
+                timesMetFirst[num]++;
+            }
+            else
+            {
+                timesMetFirst.Add(num, 1);
+            }
+
+        }
+        for (int i = 0; i < m; i++)
+        {
+            int num = int.Parse(Console.ReadLine());
+            if (timesMetSecond.ContainsKey(num))
+            {
+                timesMetSecond[num]++;
+            }
+            else
+            {
+                timesMetSecond.Add(num, 1);
+            }
+
+        }
+        foreach(var item in timesMetFirst)
+        {
+            if(timesMetSecond.ContainsKey(item.Key))
+            {
+                
+                    Console.Write(item.Key+" ");
+                
+            }    
+        }
+    }
+}
