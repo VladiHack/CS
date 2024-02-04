6e5eec5ca5053efc4d0935d742913c8a7b130e44
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-02-04 09:36:49 +0200

    Count Symbols

---
+++ C# Advanced/C# Advanced - Exercises/Sets and Dictionaries Advanced - Exercise/Count Symbols/Count Symbols/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        SortedDictionary<char,int> timesMetSymbol = new SortedDictionary<char,int>();
+        string read = Console.ReadLine();
+        for(int i=0;i<read.Length;i++)
+        {
+            if (timesMetSymbol.ContainsKey(read[i]))
+            {
+                timesMetSymbol[read[i]]++;
+            }
+            else
+            {
+                timesMetSymbol.Add(read[i], 1);
+            }
+        }
+        foreach (var item in timesMetSymbol)
+        {
+            Console.WriteLine(item.Key+": "+item.Value+" time/s");
+        }
+    }
+}
