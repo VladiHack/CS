7f5b5ebc21f4c6a287fc29fa8cc6eef7b96902fa
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-02-05 17:02:36 +0200

    Count Uppercase Words

---
+++ C# Advanced/C# Advanced - Exercises/Functional Programming - Lab/Count Uppercase Words/Count Uppercase Words/Program.cs
@@ -0,0 +1,8 @@
+﻿public class Porgram
+{
+    public static void Main(string[] args)
+    {
+        string[] read = Console.ReadLine().Split().ToArray();
+        Console.WriteLine(String.Join("\n", read.Where(p => p[0] >= 'A' && p[0]<='Z')));
+    }
+}
