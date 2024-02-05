fcabb6916d46481ac1df740a1692376966b66b58
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-02-05 16:55:52 +0200

    Sort Even Numbers

---
+++ C# Advanced/C# Advanced - Exercises/Functional Programming - Lab/Sort Even Numbers/Sort Even Numbers/Program.cs
@@ -0,0 +1,8 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] read = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+        Console.WriteLine(string.Join(", ", read.OrderBy(e=>e).Where(p => p % 2 == 0)));
+    }
+}
