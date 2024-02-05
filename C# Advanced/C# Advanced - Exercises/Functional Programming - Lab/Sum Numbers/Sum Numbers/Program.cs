408fc75b3ccae7cf73d4dd2945e628e5b9c6062d
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-02-05 16:57:45 +0200

    Sum Numbers

---
+++ C# Advanced/C# Advanced - Exercises/Functional Programming - Lab/Sum Numbers/Sum Numbers/Program.cs
@@ -0,0 +1,8 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] read = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+        Console.WriteLine(read.Length + "\n" + read.Sum());
+    }
+}
