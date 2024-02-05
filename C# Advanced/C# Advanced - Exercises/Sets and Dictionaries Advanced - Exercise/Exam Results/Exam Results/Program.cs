87d198465a53d5ac465c0ddb8148a20e901ede39
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-02-05 09:11:06 +0200

    Force Book and Exams Results

---
+++ C# Advanced/C# Advanced - Exercises/Sets and Dictionaries Advanced - Exercise/Exam Results/Exam Results/Program.cs
@@ -0,0 +1,83 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Dictionary<string, Dictionary<string, int>> participants = new Dictionary<string, Dictionary<string, int>>();
+        Dictionary<string,int> languages= new Dictionary<string,int>();
+        Dictionary<string,int> orderedParticipants= new Dictionary<string,int>();
+        List<string> banned= new List<string>();
+        while(true)
+        {
+            string read = Console.ReadLine();
+            if(read=="exam finished")
+            {
+                foreach(var participant in participants)
+                {
+                    if(!banned.Contains(participant.Key))
+                    {
+                        orderedParticipants.Add(participant.Key, 0);
+                        foreach(var contest in participant.Value)
+                        {
+                            orderedParticipants[participant.Key] += contest.Value;
+                        }
+                    }
+                }
+                 orderedParticipants = orderedParticipants.OrderByDescending(x => x.Value).ThenBy(x=>x.Key).ToDictionary(x => x.Key, x => x.Value);
+                Console.WriteLine("Results:");
+                foreach (var participant in orderedParticipants)
+                {
+                    Console.WriteLine(participant.Key+" | "+participant.Value);
+                }
+                Console.WriteLine("Submissions:");
+                languages=languages.OrderByDescending(x=>x.Value).ThenBy(x => x.Key).ToDictionary(x=>x.Key,x=>x.Value);
+                foreach(var language in languages)
+                {
+                    Console.WriteLine(language.Key+" - "+language.Value);
+                }
+
+
+                break;
+            }
+            string[] split = read.Split('-').ToArray();
+            if (split[1]=="banned")
+            {
+                banned.Add(split[0]);
+            }
+            else
+            {
+                string name = split[0]; string language= split[1]; int points = int.Parse(split[2]);
+                if(!banned.Contains(name))
+                {
+                    if (!participants.ContainsKey(name))
+                    {
+
+                        Dictionary<string, int> newParticipant = new Dictionary<string, int>();
+                        participants.Add(name, newParticipant);
+
+                    }
+                    Dictionary<string, int> currentParticipant = participants[name];
+                    if(!currentParticipant.ContainsKey(language))
+                    {
+                        currentParticipant.Add(language, points);
+                    }
+                    else if (currentParticipant[language]<points)
+                    {
+                        currentParticipant[language] = points;
+                    }
+                    participants[name]= currentParticipant;
+                }
+                if(languages.ContainsKey(language))
+                {
+                    languages[language]++;
+                }
+                else
+                {
+                    languages.Add(language, 1);
+                }
+                
+                
+            }
+
+        }
+    }
+}
---
+++ C# Advanced/C# Advanced - Exercises/Sets and Dictionaries Advanced - Exercise/ForeBook/ForeBook/Program.cs
@@ -0,0 +1,63 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Dictionary<string,string> people=new Dictionary<string,string>();
+        Dictionary<string,List<string>> sidesMembers = new Dictionary<string, List<string>>();
+        while(true)
+        {
+            string read = Console.ReadLine();
+            if(read== "Lumpawaroo")
+            {
+                foreach(var jedi in  people)
+                {
+                    if(!sidesMembers.ContainsKey(jedi.Value))
+                    {
+                        List<string> jediList=new List<string>();
+                        sidesMembers.Add(jedi.Value, jediList);
+                    }
+                    sidesMembers[jedi.Value].Add(jedi.Key);
+                }
+                var newOrder=sidesMembers.OrderByDescending(x => x.Value.Count).ThenBy(p=>p.Key).ToList();
+                foreach(var side in newOrder)
+                {
+                    if (side.Value.Count != 0)
+                    {
+                        Console.WriteLine($"Side: {side.Key}, Members: {side.Value.Count}");
+                        List<string> currentSide = side.Value;
+                        currentSide.Sort();
+                        for(int i=0;i<currentSide.Count;i++)
+                        {
+                            Console.WriteLine("! " + currentSide[i]);
+                        }
+                    }
+
+                }
+                break;
+            }
+            string name = "";string force = "";
+            read = read.Trim();
+            if (read.Contains("|"))
+            {
+                string[] split = read.Split("|").ToArray();
+                name = split[1].Trim(); force = split[0].Trim(); 
+            }
+            else
+            {
+                string[] split = read.Split("->").ToArray();
+                 name = split[0].Trim();  force= split[1].Trim();
+                Console.WriteLine($"{name} joins the {force} side!");
+
+            }
+            if (!people.ContainsKey(name))
+            {
+                people.Add(name, force);
+           
+            }
+            else
+            {
+                people[name] = force;
+            }
+        }
+    }
+}
