be69311e557be69719902d982f707bb757bf86cd
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-02-06 09:38:44 +0200

    Knights of Honor

---
+++ C# Advanced/C# Advanced - Exercises/Functional Programming - Exercise/Knights of Honor/Knights of Honor/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        string[] names = Console.ReadLine().Split().ToArray();
+        for (int i = 0; i < names.Length; i++)
+        {
+            Console.WriteLine($"Sir {names[i]}");
+        }
+    }
+}
