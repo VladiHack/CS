940b41881342a7b3cdaf9b895629761607c4e2e3
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-02-06 09:15:30 +0200

    Filter By Age

---
+++ C# Advanced/C# Advanced - Exercises/Functional Programming - Lab/Filter By Age/Filter By Age/Program.cs
@@ -0,0 +1,38 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        Dictionary<string,int> nameAge=new Dictionary<string,int>();
+        for(int i=0;i<n; i++)
+        {
+            string[] read = Console.ReadLine().Split(", ").ToArray();
+            nameAge.Add(read[0], int.Parse(read[1]));
+
+        }
+        string olderOrYounger=Console.ReadLine();
+        int age = int.Parse(Console.ReadLine());
+        string[] parameters = Console.ReadLine().Split().ToArray();
+            foreach (var item in nameAge)
+            {
+                if ((olderOrYounger == "older" && age <= item.Value) || (olderOrYounger == "younger" && age >= item.Value))
+                {
+                    if (parameters.Length == 2)
+                    {
+                        Console.WriteLine(item.Key + " - " + item.Value);
+                    }
+                    else if (parameters[0] == "name")
+                    {
+
+                        Console.WriteLine(item.Key);
+                    }
+                    else
+                    {
+                        Console.WriteLine(item.Value);
+                    }
+                }
+               
+            }
+        
+    }
+}
