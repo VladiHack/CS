acf39af44f9602a552baf48b092343d133a6f45c
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-02-06 08:55:35 +0200

    Add Vat

---
+++ C# Advanced/C# Advanced - Exercises/Functional Programming - Lab/Add VAT/Add VAT/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        double[] arr = Console.ReadLine().Split(", ").Select(double.Parse).ToArray();
+        for (int i=0;i<arr.Length; i++)
+        {
+            double num = arr[i]*1.2;
+            Console.WriteLine($"{num:F2}");
+        }
+    }
+}
