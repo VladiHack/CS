6a8f0ef79e395b58c57b8d29066b96fbda93770b
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-02-06 09:57:14 +0200

    Find Evens or Odds

---
+++ C# Advanced/C# Advanced - Exercises/Functional Programming - Exercise/Find Evens or Odds/Find Evens or Odds/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] parameters = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        string oddOrEven=Console.ReadLine();
+        for(int i = parameters[0]; i <= parameters[1];i++)
+        {
+            if (oddOrEven == "even")
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write(i+" ");
+                }
+            }
+            else
+            {
+                if(i% 2 == 1)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
