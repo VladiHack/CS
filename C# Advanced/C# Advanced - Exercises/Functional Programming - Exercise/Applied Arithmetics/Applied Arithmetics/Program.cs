234524344dda5198961cecba46463e3f8f9da599
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-02-06 10:16:52 +0200

    Applied Arithmetics

---
+++ C# Advanced/C# Advanced - Exercises/Functional Programming - Exercise/Applied Arithmetics/Applied Arithmetics/Program.cs
@@ -0,0 +1,37 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        while(true)
+        {
+            string read = Console.ReadLine();
+            if (read == "end")
+            {
+                break;
+            }
+            else
+            {
+                if(read=="add")
+                {
+                    for (int i = 0; i < nums.Length; i++)
+                        nums[i]++;
+                }
+                else if(read=="multiply")
+                {
+                    for (int i = 0; i < nums.Length; i++)
+                        nums[i] *= 2;
+                }
+                else if(read=="subtract")
+                {
+                    for (int i = 0; i < nums.Length; i++)
+                        nums[i]--;
+                }
+                else if(read=="print")
+                {
+                    Console.WriteLine(String.Join(" ",nums));
+                }
+            }
+        }
+    }
+}
