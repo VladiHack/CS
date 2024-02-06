13f3e3d2c2f4b7cec9dde5c0c48e71bd5e59bdff
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-02-06 09:31:33 +0200

    Action Print

---
+++ C# Advanced/C# Advanced - Exercises/Functional Programming - Exercise/Action Print/Action Print/Program.cs
@@ -0,0 +1,8 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        string[] names = Console.ReadLine().Split().ToArray();
+        Console.WriteLine(string.Join("\n",names));
+    }
+}
