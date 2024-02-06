d4c5710773e45e08e4e454789c39b06c6eb93add
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-02-06 10:39:27 +0200

    Predicate For Names

---
+++ C# Advanced/C# Advanced - Exercises/Functional Programming - Exercise/Predicate Of Names/Predicate Of Names/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+         int length=int.Parse(Console.ReadLine());
+        string[] names = Console.ReadLine().Split().ToArray();
+        for(int i=0;i<names.Length; i++)
+        {
+            if (names[i].Length<=length)
+            {
+                Console.WriteLine(names[i]);
+            }
+        }
+    }
+}
