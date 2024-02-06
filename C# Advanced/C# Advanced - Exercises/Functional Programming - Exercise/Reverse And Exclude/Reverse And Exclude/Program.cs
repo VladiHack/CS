b6b27e3a53426d3f5b6998383108c1eee12c3c8a
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-02-06 10:29:12 +0200

    Reverse And Exclude

---
+++ C# Advanced/C# Advanced - Exercises/Functional Programming - Exercise/Reverse And Exclude/Reverse And Exclude/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int divisors = int.Parse(Console.ReadLine());
+        for(int i=arr.Length-1; i>=0; i--)
+        {
+            if (arr[i] % divisors !=0)
+                Console.Write(arr[i]+" ");
+        }
+    }
+}
