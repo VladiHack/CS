42360f6569282fbd74c79380136f6b1a4845c816
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-02-07 11:28:49 +0200

    TriFunction

---
+++ C# Advanced/C# Advanced - Exercises/Functional Programming - Exercise/TriFunction/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int pointsToCover = int.Parse(Console.ReadLine());
+        string[] names = Console.ReadLine().Split().ToArray();
+        for (int i = 0; i < names.Length; i++)
+        {
+            int points = 0;
+            for(int s = 0; s < names[i].Length; s++)
+            {
+                points += names[i][s];
+            }
+            if(points>=pointsToCover)
+            {
+                Console.WriteLine(names[i]);
+                return;
+            }
+        }
+    }
+}
