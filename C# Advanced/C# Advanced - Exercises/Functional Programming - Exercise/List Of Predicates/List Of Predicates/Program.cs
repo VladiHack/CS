30a2f81dbbb313dbf75f0e859427a75967aa73d4
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-02-07 11:02:18 +0200

    List of Predicates

---
+++ C# Advanced/C# Advanced - Exercises/Functional Programming - Exercise/List Of Predicates/List Of Predicates/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] divisors=Console.ReadLine().Split().Select(int.Parse).ToArray();
+        for (int i = divisors.Max(); i <= n; i++)
+        {
+            if (i >= divisors.Max())
+            {
+                bool pass = true;
+                for(int s=0;s < divisors.Length;s++)
+                {
+                    if (i % divisors[s] != 0)
+                    {
+                        pass = false;
+                        break;
+                    }
+                }
+                if(pass)
+                    Console.Write(i+" ");
+            }
+        }
+    }
+}
---
+++ C# Advanced/C# Advanced - Exercises/Functional Programming - Exercise/Predicate Party/Predicate Party/Program.cs
@@ -0,0 +1,8 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<string> names = Console.ReadLine().Split().ToList();
+        
+    }
+}
