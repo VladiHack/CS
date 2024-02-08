091e1dc9fc58fcdf56b2013b81127292e3b36241
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-02-08 11:33:03 +0200

    Recursive Factorial

---
+++ C# Advanced/C# Advanced - Exercises/Algorithms/Recursive Factorial/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Program
+{
+    static long fact = 1;
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        Factorial(n);
+        Console.WriteLine(fact);
+    }
+    public static void Factorial(int n)
+    {
+        if(n==1)
+        {
+            return;
+        }
+        fact *= n;
+        Factorial(n - 1);
+    }
+}
