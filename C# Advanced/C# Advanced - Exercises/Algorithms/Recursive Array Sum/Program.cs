4301ba2601b86015538afc6cad81d59525c312c0
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-02-08 11:28:57 +0200

    Recursive Array Sum

---
+++ C# Advanced/C# Advanced - Exercises/Algorithms/Recursive Array Sum/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Program
+{
+    static int sum = 0;
+   public static void Main(string[] args)
+    {
+        int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        Recursion(arr, 0);
+        Console.WriteLine(sum);
+    }
+    public static void Recursion(int[] arr,int index)
+    {
+       
+        if(index==arr.Length)
+        {
+            return;
+        }
+        sum += arr[index];
+        Recursion(arr, index + 1);
+    }
+}
