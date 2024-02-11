e280fe028851d412a6ba31e505b1414376f70e36
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-02-11 08:05:40 +0200

    User database C#

---
+++ DataBaseApplications/UserDBApp/Model/User.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserDBApp.Model
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string Username { get; set; }
+
+        [Column(TypeName = "int")]
+        public int Age { get; set; }
+
+        [Column(TypeName = "nvarchar(200)")]
+        public string City { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id:{UserId}, username: {Username}, age: {Age}, city:{City}";
+        }
+    }
+}
