f2ec1950ae2a81f3c1d35ff8d94a4d6db03dd4f4
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-02-12 11:19:30 +0200

    Car Extension
    Added 2 new fields (fuelQuantity and fuelQuality). Changes have been made to the program.cs

--- C# Advanced/C# Advanced - Exercises/Defining Classes - Lab/CarManufacturer/Car.cs
+++ C# Advanced/C# Advanced - Exercises/Defining Classes - Lab/CarManufacturer/Car.cs
@@ -6,17 +6,27 @@ using System.Threading.Tasks;
 
 namespace CarManufacturer
 {
-    internal class Car
+    public class Car
     {
         private string make;
         private string model;
         private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
 
         public Car(string make, string model, int year)
         {
             Make = make;
             Model = model;
             Year = year;
+        }
+        public Car(string make, string model, int year,double fuelQuantity,double fuelConsumption)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            FuelConsumption = fuelConsumption;
+           FuelQuantity = fuelQuantity;
         }
 
         public string Make
@@ -49,13 +59,46 @@ namespace CarManufacturer
             }
             set 
             {
-                if(year>0)
+                if(value>0)
                 this.year=value;
                 else
                 {
                     throw new ArgumentException("Year can't be negative!");
                 }
             }
+        }
+        public double FuelQuantity
+        {
+            get
+            {
+                return this.fuelQuantity;
+            }
+            set
+            {
+                if (value > 0)
+                    this.fuelQuantity = value;
+                else
+                    throw new ArgumentException("Fuel Quantity can't be negative!");
+            }
+        }
+        public double FuelConsumption
+        {
+            get
+            {
+                return this.fuelConsumption;
+            }
+            set
+            {
+                if (value > 0)
+                    this.fuelConsumption = value;
+                else
+                    throw new ArgumentException("Fuel Consumption can't be negative!");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Car make:{this.make}, Car model:{this.model}, Car Year:{this.Year}, Car FuelQuantity:{this.fuelQuantity:F2}, Car FuelConsumption: {this.FuelConsumption:F2}";
         }
     }
 }
--- C# Advanced/C# Advanced - Exercises/Defining Classes - Lab/CarManufacturer/Program.cs
+++ C# Advanced/C# Advanced - Exercises/Defining Classes - Lab/CarManufacturer/Program.cs
@@ -3,9 +3,30 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Enter car parameters in the format : Make, Model, Year");
-        string[] parameters = Console.ReadLine().Split(", ").ToArray();
-        Car car= new Car(parameters[0], parameters[1], int.Parse(parameters[2]));
+        try
+        {
+            Console.WriteLine("Enter car parameters in the format : Make, Model, Year\nor : Make, Model, Year, FuelQuantity, FuelConsumption");
+            string[] parameters = Console.ReadLine().Split(", ").ToArray();
+            if (parameters.Length >= 3)
+            {
+                Car car = new Car(parameters[0], parameters[1], int.Parse(parameters[2]));
+                if (parameters.Length == 5)
+                {
+                    double fuelQuantity = double.Parse(parameters[3]);
+                    double fuelConsumption = double.Parse(parameters[4]);
+                    car.FuelConsumption = fuelConsumption;
+                    car.FuelQuantity = fuelQuantity;
+                }
+                Console.WriteLine(car);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            return;
+        }
+       
+       
 
     }
 }
