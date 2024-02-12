af292686ab4ea2779af5fb9c0baf7caf7bee861c
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-02-12 11:06:58 +0200

    Car project Step 1
    Defining Class Car and creating an instance in the Main method

---
+++ C# Advanced/C# Advanced - Exercises/Defining Classes - Lab/CarManufacturer/Car.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    internal class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+        public string Make
+        {
+            get
+            {
+                return make;
+            }
+            set
+            {
+                this.make= value;
+            }
+        }
+        public string Model
+        {
+            get
+            {
+                return this.model;
+            }
+            set
+            {
+                this.model = value;
+            }
+        }
+        public int Year
+        {
+            get
+            {
+                return this.year;
+            }
+            set 
+            {
+                if(year>0)
+                this.year=value;
+                else
+                {
+                    throw new ArgumentException("Year can't be negative!");
+                }
+            }
+        }
+    }
+}
---
+++ C# Advanced/C# Advanced - Exercises/Defining Classes - Lab/CarManufacturer/Program.cs
@@ -0,0 +1,11 @@
+﻿using CarManufacturer;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter car parameters in the format : Make, Model, Year");
+        string[] parameters = Console.ReadLine().Split(", ").ToArray();
+        Car car= new Car(parameters[0], parameters[1], int.Parse(parameters[2]));
+
+    }
+}
